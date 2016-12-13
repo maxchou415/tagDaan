2ef253785ddf7fd16e955fb16a8bea517b49efd2
Yi-Hsun Chou <Yi-HsunChou@Yi-Hsuns-MacBook-Air.local>
2016-12-14 07:23:11 +0800

    - Tag 你身邊的橘人😆

---
+++ app.js
@@ -0,0 +1,61 @@
+var http = require('http')
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var FB = require('fb')
+var cors = require('cors');
+
+
+var app = express();
+var server = http.createServer(app);
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(cors());
+
+server.listen(8000, '0.0.0.0', function() {
+  console.log('Ready on http://localhost:8000');
+}); //Run Server On port 8080
+
+
+app.get('/', function(req, res) {
+  res.render('index.ejs', {
+    title: '靠北景文高中' // Insert Your Facebook Page Name
+  });
+});
+
+//Post Page
+app.route('/add')
+  .post(function(req, res, next) {
+    var message = req.param('message');
+    var response = {};
+    FB.setAccessToken(''); //Insert Your Acccess Token
+    FB.api('kaobeiJWSH/feed', 'post', {
+      message: message
+    }, function(res) {
+      if (!res || res.error) {
+        console.log(!res ? 'error occurred' : res.error);
+        return;
+      }
+      console.log('Post Id: ' + res.id);
+    });
+
+    if (res.id == 0) {
+      response = ({"message" : "failed"});
+    } else {
+      var id = res.id;
+      response = ({"message" : "success", "id" : id});
+
+    }
+
+
+    res.json(response);
+
+  })
+//Error Page
+app.use(function(req, res) {
+  res.send("404: Page not Found", 404);
+});
